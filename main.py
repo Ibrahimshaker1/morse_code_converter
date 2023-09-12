21f16ad7c4778647b40301425a545f22d66304cb
Ibrahimshaker1 <ibrahimshaker829@gmail.com>
2023-09-11 17:32:17 -0800

    edit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from filter import Filter
 from converter import Convert
-path = input("please past your txt file path here: ")
+path = input("please paste your txt file path here: ")
 my_filter = Filter(path)
 path = my_filter.valid_path()
 filter_massage = my_filter.read(path)
